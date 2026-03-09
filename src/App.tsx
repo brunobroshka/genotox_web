ef1f53ae45c4d02576b834c9bf662fb20c5df615
AdrianCabreraPhi <adriancabproven@gmail.com>
2026-03-09 11:16:34 +0100

    added heatmap in result and motion.div implemented

--- src/App.tsx
+++ src/App.tsx
@@ -3,40 +3,31 @@ import Header from "./components/Header";
 import Form from "./components/Form";
 import { useState } from "react";
 import Footer from "./components/Footer";
-import useDownloadData from "./hooks/useDownloadData";
 import ButtonDownloadData from "./components/ButtonDownloadData";
 import ResultDashboard from "./components/ResultDashboard";
 
 function App() {
   const [result, setResult] = useState<object | boolean>(false);
-  const [submittedCas, setSubmittedCas] = useState("");
-
-  const { downloadData } = useDownloadData();
+  const [submittedCas, setSubmittedCas] = useState<string>("");
 
   const handleSubmittedCasNum = (casNum: string) => {
     setSubmittedCas(casNum);
-  };
-
-  const handleDownloadData = () => {
-    downloadData(submittedCas, result);
   };
 
   return (
     <>
       <div className="min-h-screen w-full bg-[linear-gradient(to_bottom,#fff_0%,#fff_40%,#064e3b_100%)] flex flex-col">
         <Header />
-
         <div className="components  flex flex-col bg-white w-auto transition-all ease-in-out duration-300 shadow-2xl flex-grow m-8 rounded-md">
           <div className="relative group flex flex-col md:flex-row justify-between w-full px-4 py-4 gap-4 items-start bg-neutral-50   rounded-t-md">
             <Form
               setResult={setResult}
               handleSubmittedCasNum={handleSubmittedCasNum}
             />
-            <ButtonDownloadData downloadData={handleDownloadData} />
+            <ButtonDownloadData result={result} submittedCas={submittedCas}  />
           </div>
           <ResultDashboard submittedCas={submittedCas} result={result} />
         </div>
-
         <Footer />
       </div>
     </>
--- src/components/ResultDashboard.tsx
+++ src/components/ResultDashboard.tsx
@@ -3,8 +3,14 @@ import PieChart from "./pieChart";
 import { AnimatePresence, motion } from "framer-motion";
 import ModalInformationDatabase from "./ModalInformationDatabase";
 import { useState } from "react";
+import HeatMapChart from "./HeatMapChart";
 
-function ResultDashboard({ submittedCas, result }) {
+interface ResultDashboardProps {
+  submittedCas: string;
+  result: any;
+}
+
+function ResultDashboard({ submittedCas, result }: ResultDashboardProps) {
   const [hoverEffectDatabases, setHoverEffectDatabases] = useState<object>({});
   const [selectedDatabase, setSelectedDatabase] = useState<string | undefined>(
     undefined,
@@ -18,28 +24,38 @@ function ResultDashboard({ submittedCas, result }) {
 
   const closeModalDataBaseInformation = () => {
     setSelectedDatabase(undefined);
-  }
+  };
 
-  
   return (
     <>
-      <div className="flex-1  overflow-hidden">
-        {result && (
-          <>
-            <VisualizeData
-              setSelectedDatabase={setSelectedDatabase}
-              hoverEffectDatabases={hoverEffectDatabases}
-              result={result}
-              selectedDatabase={selectedDatabase}
-              handleChangeDatabase={handleChangeDatabase}
-            />
-            <PieChart
-              casNum={submittedCas}
-              setHoverEffectDatabases={setHoverEffectDatabases}
-            />
-          </>
-        )}
-      </div>
+      {result && (
+        <motion.div
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          className="flex-1 overflow-hidden"
+        >
+
+          <VisualizeData
+            setSelectedDatabase={setSelectedDatabase}
+            hoverEffectDatabases={hoverEffectDatabases}
+            result={result}
+            selectedDatabase={selectedDatabase}
+            handleChangeDatabase={handleChangeDatabase}
+          />
+
+          <div className="flex flex-col justify-center items-center lg:flex-row flex-1 min-h-0">
+            <div className="flex-1 min-w-0 overflow-auto">
+              <PieChart
+                casNum={submittedCas}
+                setHoverEffectDatabases={setHoverEffectDatabases}
+              />
+            </div>
+            <div className="flex-1 min-w-0 overflow-auto">
+              <HeatMapChart casNum={submittedCas} />
+            </div>
+          </div>
+        </motion.div>
+      )}
 
       <AnimatePresence>
         {result && selectedDatabase && (
