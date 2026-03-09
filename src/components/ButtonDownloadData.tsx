4aae7fe4abbf480f1f26637ea262790103ab2d09
AdrianCabreraPhi <adriancabproven@gmail.com>
2026-03-09 11:15:12 +0100

    add logic close to component

--- src/components/ButtonDownloadData.tsx
+++ src/components/ButtonDownloadData.tsx
@@ -1,13 +1,20 @@
 import { FaFileDownload } from "react-icons/fa";
+import useDownloadData from "../hooks/useDownloadData";
 
 type ButtonDownloadDataProps = {
-  downloadData: () => void
-}
+  result: object | boolean;
+  casNum: string;
+};
+
 function ButtonDownloadData(props: ButtonDownloadDataProps) {
+
+  const { result, casNum } = props;
+  const { downloadData } = useDownloadData();
 
   return (
     <button
-      onClick={props.downloadData}
+      disabled={!result}
+      onClick={() => downloadData(casNum, result)}
       className="rounded-md border disabled:grayscale grayscale-0 disabled:opacity-50 text-green-600 disabled:cursor-not-allowed p-1 px-2 shadow-md transition-all ease-in-out duration-300 hover:bg-gray-100 transform hover:-translate-y-1 active:-translate-y-0 font-semibold"
     >
       <FaFileDownload size={20} />
